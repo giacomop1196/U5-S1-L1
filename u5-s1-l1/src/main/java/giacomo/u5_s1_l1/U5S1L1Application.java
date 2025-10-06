02c64f3239309dd6f8f3e0ad9f9b8240df6374bc
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-10-06 13:49:16 +0200

    first commit

---
+++ u5-s1-l1/src/main/java/giacomo/u5_s1_l1/U5S1L1Application.java
@@ -0,0 +1,13 @@
+package giacomo.u5_s1_l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S1L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S1L1Application.class, args);
+	}
+
+}
---
+++ u5-s1-l1/src/test/java/giacomo/u5_s1_l1/U5S1L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package giacomo.u5_s1_l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S1L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
